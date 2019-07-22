8d404cd726e0933946466a071f7363e38dcdb7ab
sg <srfngrc@outlook.com>
2019-07-22 17:17:33 -0400

    1st upload july22th 5h17pm

---
+++ ConnectToAdb_Window.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace HowToConnectToaDB_basics
+{
+    partial class ConnectToAdb_Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.lblisin = new System.Windows.Forms.Label();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(135, 145);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 0;
+            this.btnInsert.Text = "INSERT";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            // 
+            // lblisin
+            // 
+            this.lblisin.AutoSize = true;
+            this.lblisin.Location = new System.Drawing.Point(21, 45);
+            this.lblisin.Name = "lblisin";
+            this.lblisin.Size = new System.Drawing.Size(34, 13);
+            this.lblisin.TabIndex = 1;
+            this.lblisin.Text = "ISIN: ";
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Location = new System.Drawing.Point(21, 87);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(49, 13);
+            this.lblAmount.TabIndex = 2;
+            this.lblAmount.Text = "Amount: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(89, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(146, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // ConnectToAdb_Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 190);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.lblisin);
+            this.Controls.Add(this.btnInsert);
+            this.Name = "ConnectToAdb_Window";
+            this.Text = "New Operation Insert";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Label lblisin;
+        private System.Windows.Forms.Label lblAmount;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
